3698377d54d4d3eb04973db3ffd11dce66bfcce0
taiyu <tone.ai0717@gmail.com>
2026-05-26 21:19:21 +0900

    fix: キャッシュ無効化でアプリ更新できるように

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+        ],
+      },
+      {
+        source: "/manifest.json",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
